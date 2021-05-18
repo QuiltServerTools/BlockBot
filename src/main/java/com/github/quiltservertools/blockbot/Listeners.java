6d51db1956e1034f531205323894422f9baf3ff2
yitzy299 <yitzy299@noreply.github.com>
2021-05-18 21:10:07 +0100

    Improve image handling
    - Click on the link in chat to open the attached image
    - Text sent below
    
    Signed-off-by: yitzy299 <yitzy299@noreply.github.com>

--- src/main/java/com/github/quiltservertools/blockbot/Listeners.java
+++ src/main/java/com/github/quiltservertools/blockbot/Listeners.java
@@ -7,10 +7,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.minecraft.network.MessageType;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TextColor;
+import net.minecraft.text.*;
 import net.minecraft.util.Util;
 import org.jetbrains.annotations.NotNull;
 
@@ -48,10 +45,16 @@ public class Listeners extends ListenerAdapter {
     }
 
     private void sendMessageToGame(MinecraftServer server, MessageReceivedEvent event) {
-        String msg = event.getMessage().getAttachments().isEmpty() ? event.getMessage().getContentDisplay() : "[Image] " + event.getMessage().getContentDisplay();
+        String msg = event.getMessage().getContentDisplay();
         int colour = Objects.requireNonNull(event.getMember()).getColorRaw();
         Text message = new LiteralText(msg);
         MutableText sender = new LiteralText("<@").append(new LiteralText(event.getMember().getEffectiveName()).styled(style -> style.withColor(TextColor.fromRgb(colour)))).append(new LiteralText("> "));
-        server.getPlayerManager().broadcastChatMessage(sender.append(message), MessageType.SYSTEM, Util.NIL_UUID);
+        if (!event.getMessage().getAttachments().isEmpty()) {
+            event.getMessage().getAttachments().forEach(attachment -> server.getPlayerManager().broadcastChatMessage(sender.append(new LiteralText(attachment.getUrl()).styled(s -> s.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, attachment.getUrl())))), MessageType.SYSTEM, Util.NIL_UUID));
+            if (!msg.equals("")) server.getPlayerManager().broadcastChatMessage(sender.append(message), MessageType.SYSTEM, Util.NIL_UUID);
+        } else {
+            server.getPlayerManager().broadcastChatMessage(sender.append(message), MessageType.SYSTEM, Util.NIL_UUID);
+        }
+
     }
 }
