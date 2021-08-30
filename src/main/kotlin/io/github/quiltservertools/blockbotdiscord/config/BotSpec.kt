140d5d40035238be65b4ee3b70784d48bb2dca4f
PotatoPresident <potatoboy9999@outlook.com>
2021-08-30 13:14:00 -0700

    Move config helpers

--- src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/BotSpec.kt
+++ src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/BotSpec.kt
@@ -1,9 +1,33 @@
 package io.github.quiltservertools.blockbotdiscord.config
 
+import com.google.common.collect.BiMap
+import com.google.common.collect.HashBiMap
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.behavior.getChannelOf
+import dev.kord.core.entity.channel.GuildMessageChannel
+import io.github.quiltservertools.blockbotdiscord.logWarn
 
 object BotSpec : ConfigSpec() {
     val token by required<String>()
     val guild by required<Long>()
     val channels by required<Map<String, Long>>()
 }
+
+fun Config.getChannelsBi(): BiMap<String, Long> = HashBiMap.create(this[BotSpec.channels])
+
+suspend fun Config.getChannel(name: String, bot: ExtensibleBot): GuildMessageChannel {
+    val channel: GuildMessageChannel? =
+        this[BotSpec.channels][name]?.let { Snowflake(it) }?.let { this.getGuild(bot).getChannelOf(it) }
+    if (channel == null) {
+        logWarn("Invalid channel '${name}'. Make sure it is defined and correct in your config")
+    }
+
+    return channel!!
+}
+
+suspend fun Config.getGuild(bot: ExtensibleBot) = bot.getKoin().get<Kord>().getGuild(Snowflake(this[BotSpec.guild]))!!
+
--- src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/ChatRelaySpec.kt
+++ src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/ChatRelaySpec.kt
@@ -1,6 +1,21 @@
 package io.github.quiltservertools.blockbotdiscord.config
 
+import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
+import dev.kord.core.entity.Message
+import eu.pb4.placeholders.PlaceholderAPI
+import eu.pb4.placeholders.TextParser
+import io.github.quiltservertools.blockbotapi.sender.MessageSender
+import io.github.quiltservertools.blockbotapi.sender.PlayerMessageSender
+import io.github.quiltservertools.blockbotdiscord.utility.literal
+import io.github.quiltservertools.blockbotdiscord.utility.summary
+import net.minecraft.advancement.Advancement
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import java.util.*
 
 object ChatRelaySpec : ConfigSpec() {
 
@@ -33,3 +48,110 @@ object ChatRelaySpec : ConfigSpec() {
         val playerAvatarUrl by required<String>()
     }
 }
+
+fun Config.formatDiscordMessage(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordMessageFormatSpec.messageFormat])
+
+fun Config.formatDiscordEmote(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordMessageFormatSpec.emoteFormat])
+
+fun Config.formatDiscordAnnouncement(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordMessageFormatSpec.announcementFormat])
+
+fun Config.formatWebhookMessage(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordWebhookFormatSpec.messageFormat])
+
+fun Config.formatWebhookEmote(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordWebhookFormatSpec.emoteFormat])
+
+fun Config.formatWebhookAnnouncement(sender: MessageSender, message: String): String =
+    formatDiscordRelayMessage(sender, message, config[ChatRelaySpec.DiscordWebhookFormatSpec.announcementFormat])
+
+fun Config.formatPlayerJoinMessage(player: ServerPlayerEntity): String =
+    formatDiscordRelayMessage(player, config[ChatRelaySpec.DiscordMessageFormatSpec.playerJoin])
+
+fun Config.formatPlayerLeaveMessage(player: ServerPlayerEntity): String =
+    formatDiscordRelayMessage(player, config[ChatRelaySpec.DiscordMessageFormatSpec.playerLeave])
+
+fun Config.formatPlayerAdvancementMessage(player: ServerPlayerEntity, advancement: Advancement): String =
+    formatDiscordRelayMessage(player, config[ChatRelaySpec.DiscordMessageFormatSpec.playerAdvancement], mapOf("advancement" to advancement.display!!.title))
+
+fun Config.formatServerStartMessage(server: MinecraftServer): String =
+    formatDiscordRelayMessage(server, config[ChatRelaySpec.DiscordMessageFormatSpec.serverStart])
+
+fun Config.formatServerStopMessage(server: MinecraftServer): String =
+    formatDiscordRelayMessage(server, config[ChatRelaySpec.DiscordMessageFormatSpec.serverStop])
+
+private fun formatDiscordRelayMessage(sender: MessageSender, message: String, format: String): String =
+    PlaceholderAPI.parseText(
+        PlaceholderAPI.parsePredefinedText(
+            format.literal(),
+            PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+            mapOf(
+                "sender" to sender.name,
+                "sender_display" to sender.displayName,
+                "message" to message.literal()
+            )
+        ),
+        if (sender is PlayerMessageSender) sender.player else null
+    ).string
+
+fun formatDiscordRelayMessage(player: ServerPlayerEntity, format: String, placeholders: Map<String, Text> = mapOf()): String =
+    PlaceholderAPI.parseText(
+        PlaceholderAPI.parsePredefinedText(
+            format.literal(),
+            PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+            placeholders
+        ),
+        player
+    ).string
+
+fun formatDiscordRelayMessage(server: MinecraftServer, format: String, placeholders: Map<String, Text> = mapOf()): String =
+    PlaceholderAPI.parseText(
+        PlaceholderAPI.parsePredefinedText(
+            format.literal(),
+            PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+            placeholders
+        ),
+        server
+    ).string
+
+fun Config.getMinecraftChatRelayMsg(
+    sender: MutableText,
+    topRole: MutableText,
+    message: Text,
+    server: MinecraftServer
+): Text = PlaceholderAPI.parseText(
+    PlaceholderAPI.parsePredefinedText(
+        TextParser.parse(this[ChatRelaySpec.MinecraftFormatSpec.messageFormat]),
+        PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+        mapOf(
+            "sender" to sender.copy().formatted(Formatting.RESET),
+            "sender_colored" to sender,
+            "top_role" to topRole,
+            "message" to message
+        )
+    ), server
+)
+
+fun Config.getReplyMsg(
+    sender: String,
+    message: Message,
+    server: MinecraftServer
+): Text = PlaceholderAPI.parseText(
+    PlaceholderAPI.parsePredefinedText(
+        TextParser.parse(this[ChatRelaySpec.MinecraftFormatSpec.replyFormat]),
+        PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+        mapOf(
+            "sender" to (sender).literal(),
+            "summary" to message.summary().literal(),
+        )
+    ), server
+)
+
+fun Config.getWebhookChatRelayAvatar(uuid: UUID): String =
+    PlaceholderAPI.parsePredefinedText(
+        this[ChatRelaySpec.WebhookSpec.playerAvatarUrl].literal(),
+        PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
+        mapOf("uuid" to uuid.toString().literal())
+    ).string
