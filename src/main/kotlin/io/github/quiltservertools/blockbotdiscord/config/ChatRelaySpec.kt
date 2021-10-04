48687588e21f583ff007b08c5946926a036b9195
PotatoPresident <51728317+PotatoPresident@users.noreply.github.com>
2021-10-04 09:59:19 -0700

    Add `uuid` and `texture` placeholders to player avatar config

--- src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/ChatRelaySpec.kt
+++ src/main/kotlin/io/github/quiltservertools/blockbotdiscord/config/ChatRelaySpec.kt
@@ -1,5 +1,6 @@
 package io.github.quiltservertools.blockbotdiscord.config
 
+import com.mojang.authlib.GameProfile
 import com.uchuhimo.konf.Config
 import com.uchuhimo.konf.ConfigSpec
 import dev.kord.core.entity.Message
@@ -7,11 +8,13 @@ import eu.pb4.placeholders.PlaceholderAPI
 import eu.pb4.placeholders.TextParser
 import io.github.quiltservertools.blockbotapi.sender.MessageSender
 import io.github.quiltservertools.blockbotapi.sender.PlayerMessageSender
+import io.github.quiltservertools.blockbotdiscord.utility.getTextures
 import io.github.quiltservertools.blockbotdiscord.utility.literal
 import io.github.quiltservertools.blockbotdiscord.utility.summary
 import net.minecraft.advancement.Advancement
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.LiteralText
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
@@ -174,9 +177,13 @@ fun Config.getReplyMsg(
     ), server
 )
 
-fun Config.getWebhookChatRelayAvatar(uuid: UUID): String =
+fun Config.getWebhookChatRelayAvatar(gameProfile: GameProfile): String =
     PlaceholderAPI.parsePredefinedText(
         this[ChatRelaySpec.WebhookSpec.playerAvatarUrl].literal(),
         PlaceholderAPI.ALT_PLACEHOLDER_PATTERN_CUSTOM,
-        mapOf("uuid" to uuid.toString().literal())
+        mapOf(
+            "uuid" to gameProfile.id.toString().literal(),
+            "username" to gameProfile.name.literal(),
+            "texture" to (gameProfile.getTextures()?.literal() ?: LiteralText(""))
+        )
     ).string
